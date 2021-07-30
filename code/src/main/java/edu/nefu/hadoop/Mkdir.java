b8cd7ca06946c0cd49c30ecb4d54ee16397dcaa8
AB-IN-lsy <liusy200708@163.com>
2021-07-31 00:17:59 +0800

    First commit on 2021.7.31

---
+++ code/src/main/java/edu/nefu/hadoop/Mkdir.java
@@ -0,0 +1,53 @@
+package edu.nefu.hadoop;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLine;
+
+public class Mkdir 
+{
+    public static void main( String[] args ) throws IOException, Exception, URISyntaxException
+    {
+
+        CommandLineParser parser = new BasicParser( );
+        Options options = new Options( );
+        options.addOption("h", "help", false, "Print this usage information");
+        options.addOption("d", "dir", true, "Dir to make");
+        CommandLine commandLine = parser.parse( options, args );
+
+        if( commandLine.hasOption('h') ) {
+            System.out.println( "Help Message");
+            System.exit(0);
+        }
+
+        String file = "";
+        if( commandLine.hasOption('d') ) {  
+            file = commandLine.getOptionValue('d');  
+        } else {
+            System.out.println( "need input param: -d" );
+            System.exit(1);
+        }
+
+        Configuration conf = new Configuration();
+        conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+
+        //1 获取hdfs客户端对象
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop1:9000"), conf, "root");
+
+        //2 在hdfs上创建路径
+        fs.mkdirs(new Path(file));
+
+        //3 关闭资源
+        fs.close();
+        System.out.printf("HDFS mkdir: %s\n", file);
+    }
+}
